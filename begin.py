f2035ee4f7cbc00ccd4f2318f349b4e4da2a60a4
Timur <timur.umarov86@gmail.com>
2023-03-11 14:50:41 -0500

    initial commit to repo

---
+++ begin.py
@@ -0,0 +1,17 @@
+first_name = "Ada"
+# first_name = 11
+
+print(first_name)
+print("These", "will be", "joined together by spaces")
+
+print("Hello, Graig")
+print("Hello,", first_name)
+
+print("Graig is learning Python")
+print(first_name, "is learning Python")
+
+favorite_color = "Brown"
+print("The color", favorite_color, "is my favorite!")
+
+current_mood = input("How are today?  ")
+print("I am ", current_mood, "today.")
